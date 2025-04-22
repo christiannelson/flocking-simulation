d686d46c3cae345de4f0603ad872bbf3bc05ae9f
Christian Nelson <cnelson@slac.com>
2025-04-22 06:39:39 -0700

    Another tweak to bird color

--- shaders/boidGeometryFrag.glsl
+++ shaders/boidGeometryFrag.glsl
@@ -2,6 +2,10 @@ varying vec4 vertex_color;
 varying float zcoordi;
 
 void main() {
+    // Previous version that ignored color
+    //float z = 0.2 + ( 1000. - zcoordi ) / 1000. * vertex_color.x;
+    //gl_FragColor = vec4( z, z, z, 1. );
+
     // Calculate depth-based brightness factor (0.2 to 1.0 range)
     float brightness = 0.2 + (1000.0 - zcoordi) / 1000.0;
     
